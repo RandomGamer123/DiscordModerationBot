b58365afaedadc90f91f1652d16dbd054cf2f351
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2020-04-11 13:11:33 -0400

    First command

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import discord,json
 
-client = discord.Client()
-
 with open("Config/token.json") as token_file:
     tokens = json.load(token_file)
     
 with open("Config/config.json") as config_file:
     config = json.load(config_file);
-    
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -18,4 +16,13 @@ async def on_message(message):
         return
     if (message.content.startswith(config["prefix"])):
         return
-client.run(tokens["bottoken"])
+    args = (message.content[1:]).split(" ")
+    command = args.pop(0)
+    if (command == "getsource") {
+        message.channel.send("This bot is open source, the source code is at: <https://github.com/RandomGamer123/DiscordModerationBot>.")
+    }
+if getenv("BOTTOKEN"):
+    bottoken = getenv("BOTTOKEN")
+else: 
+    bottoken = tokens["bottoken"]
+client.run(bottoken)
