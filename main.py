2536b8a367c14cbce4b871d35864d19e90f070c4
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2020-04-11 13:19:55 -0400

    Fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import discord,json
+import discord,json,os
+
+client = discord.Client()
 
 with open("Config/token.json") as token_file:
     tokens = json.load(token_file)
@@ -14,15 +16,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if (message.content.startswith(config["prefix"])):
+    if not (message.content.startswith(config["prefix"])):
         return
     args = (message.content[1:]).split(" ")
     command = args.pop(0)
-    if (command == "getsource") {
-        message.channel.send("This bot is open source, the source code is at: <https://github.com/RandomGamer123/DiscordModerationBot>.")
-    }
-if getenv("BOTTOKEN"):
-    bottoken = getenv("BOTTOKEN")
+    if (command == "getsource"):
+        await message.channel.send("This bot is open source, the source code is at: <https://github.com/RandomGamer123/DiscordModerationBot>.")
+if os.getenv("BOTTOKEN"):
+    bottoken = os.getenv("BOTTOKEN")
 else: 
     bottoken = tokens["bottoken"]
 client.run(bottoken)
