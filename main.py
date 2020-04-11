e42767edb3d251b7f64f35d707b8b4c463723b98
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2020-04-11 13:47:21 -0400

    Google Sheets Update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import discord,json,os
+import discord,json,os,httplib2
+from apiclient import discovery
+from google.oauth2 import service_account
 
 client = discord.Client()
 
@@ -8,6 +10,12 @@ with open("Config/token.json") as token_file:
 with open("Config/config.json") as config_file:
     config = json.load(config_file);
 
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+secret_file = "Config/client_secret.json"
+credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
+service = discovery.build('sheets','v4',credentials=credentials)
+    
+warninglogid = tokens["warninglogid"]
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -20,8 +28,12 @@ async def on_message(message):
         return
     args = (message.content[1:]).split(" ")
     command = args.pop(0)
+    if message.author.id in [156390113654341632]: 
+        perms = 12
     if (command == "getsource"):
         await message.channel.send("This bot is open source, the source code is at: <https://github.com/RandomGamer123/DiscordModerationBot>.")
+    if (command == "testsheets" and perms >= 12):
+        service.spreadsheets().values().append(spreadsheetId = warninglogid, range = "TestSheet!A1:B2", valueInputOption = "RAW", insertDataOption = "INSERT_ROWS", body = {"values":[["testing",message.id]]}).execute()
 if os.getenv("BOTTOKEN"):
     bottoken = os.getenv("BOTTOKEN")
 else: 
